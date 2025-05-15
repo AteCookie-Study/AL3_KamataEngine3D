edd410d103a652e636f16e714186a4fbc888506e
AteCookie <k024g1012@g.neec.ac.jp>
2025-05-15 15:00:20 +0900

    1

---
+++ DirectXGame/MapChipField.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "KamataEngine.h"
+
+
+/// <summary>
+/// MapChipField.h
+///	</summary>
+
+enum class MapChipType {
+
+};
+
+class MapChipField {
+public:
+
+	// 1Block Size
+	static inline const float kBlockWidth = 1.0f;
+	static inline const float kBlockHeight = 1.0f;
+
+	//Block Quantity
+
+	static inline const uint32_t kNumBlockVirticla = 20;
+	static inline const uint32_t kNumBlockHorizontal = 100;
+
+};
